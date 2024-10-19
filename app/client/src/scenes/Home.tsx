67e351214a92779c51f5e7f0bb916c0700991896
anig1scur <anigiscur@gmail.com>
2024-10-19 17:40:18 +0800

    style: update home page

--- app/client/src/scenes/Home.tsx
+++ app/client/src/scenes/Home.tsx
@@ -2,25 +2,52 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Client } from 'colyseus.js';
 import qs from 'querystringify';
-import {
-  Box,
-  Button,
-  IListItem,
-  Inline,
-  Input,
-  Room,
-  Select,
-  Separator,
-  Space,
-  Text,
-  View,
-} from '../components';
 import { Constants, Types } from '@jouer/common';
 import { RoomAvailable } from 'colyseus.js/lib/Room';
 
 
+function Room(props: {
+  id: string;
+  roomName: string;
+  clients: number;
+  maxClients: number;
+  mode: string;
+  onClick: (id: string) => void;
+}): React.ReactElement {
+  const { id, roomName, clients, maxClients, mode, onClick } = props;
 
-const PlayersCountList: IListItem[] = Constants.ROOM_PLAYERS_SCALES.map((value) => ({
+  return (
+    <div
+      className={ `flex rounded-md cursor-pointer bg-[#D1714A] bg-opacity-10 text-[#FFEDD7] gap-2 font-jmadh text-2xl` }
+      onClick={ () => onClick(id) }
+    >
+      <div className="flex-grow flex rounded-md justify-between items-center px-2 bg-[#FFEDD7] bg-opacity-20">
+        <div title="Name"  >
+          { `${ roomName || `Unknown's room` }` }
+        </div>
+        <div title='Players' >
+          { `${ clients } / ${ maxClients }` }
+        </div>
+      </div>
+      <button
+        className="ml-auto rounded-md bg-[#D1714A] text-[#FFEDD7] py-[1px] px-4 hover:bg-[#FFEDD7] hover:text-[#D1714A]"
+        type="button"
+      >
+        Join
+      </button>
+    </div>
+  );
+}
+
+const Face = () => <svg width="100%" height="100%" viewBox="0 0 67 67" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="33.5" cy="33.5" r="32" stroke="currentColor" stroke-width="3" />
+  <circle cx="30.485" cy="23.785" r="2.515" fill="currentColor" stroke="currentColor" />
+  <circle cx="51.9249" cy="23.785" r="2.515" fill="currentColor" stroke="currentColor" />
+</svg>
+
+
+
+const PlayersCountList = Constants.ROOM_PLAYERS_SCALES.map((value) => ({
   value,
   title: `${ value } players`,
 }));
@@ -99,82 +126,75 @@ const Home: React.FC = () => {
   };
 
   const renderName = () => (
-    <Box style={ { width: 500, maxWidth: '100%' } }>
-      <View flex column>
-        <Inline size="thin" />
-        <Text>Welcome to Board Game Jouer !</Text>
-        <br />
-        <Text>Pick your name</Text>
-      </View>
-      <Space size="xs" />
-      <Input
-        value={ playerName }
-        placeholder="Name"
-        maxLength={ Constants.PLAYER_NAME_MAX }
-        onChange={ handlePlayerNameChange }
-      />
-      { hasNameChanged && (
-        <>
-          <Space size="xs" />
-          <Button title="Save" text="Save" onClick={ handleNameSave } />
-        </>
-      ) }
-    </Box>
+    <div className='bg-[#F69C6C] text-[#FFEDD7] shadow-md w-full flex flex-col gap-3 px-6 py-2 rounded-md '>
+      <div className='flex items-center  justify-between'>
+        <div className='w-16 h-16'>
+          <Face />
+        </div>
+        <div className='font-kreon mb-4 text-7xl'>𝓳  𝓸  𝓾  𝓮  𝓻</div>
+      </div>
+      <div className='flex font-jmadh items-center gap-3 text-2xl justify-between'>
+        <div className='text-4xl mb-2 '>Name</div>
+        <input
+          value={ playerName }
+          placeholder="Your Name"
+          maxLength={ Constants.PLAYER_NAME_MAX }
+          onChange={ handlePlayerNameChange }
+          className='rounded-md font-jmadh w-full px-2 placeholder:text-amber-700 placeholder:text-opacity-30 bg-white text-[#70422F]'
+        />
+        <div className='bg-dice min-w-8 w-8 h-8 bg-cover '></div>
+        { hasNameChanged && <div className='bg-[#D1714A] px-1 rounded-md w-16 min-w-16 cursor-pointer text-center' onClick={ handleNameSave }>Save</div> }
+
+      </div>
+    </div>
   );
 
   const renderNewRoom = () => (
-    <View flex style={ { alignItems: 'flex-start', flexDirection: 'column' } }>
+    <div className='flex flex-col gap-3 font-jmadh mb-3'>
       { !isNewRoom && (
-        <Button title="Create new room" text="+ New Room" onClick={ () => setIsNewRoom(true) } />
+        <div title="Create new room" className='bg-[#D1714A] px-1 rounded-md h-10 text-center text-3xl cursor-pointer' onClick={ () => setIsNewRoom(true) } >
+          Create Room
+        </div>
       ) }
       { isNewRoom && (
-        <View style={ { width: '100%' } }>
-          <Text>Name:</Text>
-          <Space size="xxs" />
-          <Input
-            placeholder="Name"
-            value={ roomName }
-            maxLength={ Constants.ROOM_NAME_MAX }
-            onChange={ handleRoomNameChange }
-          />
-          <Space size="s" />
-
-          <Text>Max players:</Text>
-          <Space size="xxs" />
-          <Select
-            value={ roomMaxPlayers }
-            values={ PlayersCountList }
-            onChange={ (event: React.ChangeEvent<HTMLSelectElement>) => {
-              setRoomMaxPlayers(Number(event.target.value));
-            } }
-          />
-          <Space size="s" />
-
-          <View>
-            <Button title="Create room" text="Create" onClick={ handleCreateRoomClick } />
-            <Space size="xs" />
-            <Button title="Cancel" text="Cancel" reversed onClick={ () => setIsNewRoom(false) } />
-          </View>
-        </View>
+        <div className='flex flex-col font-jmadh w-full text-2xl text-[#FFEDD7]'>
+          <div className='flex justify-between items-center gap-3'>
+            <label className='min-w-28 text-right text-3xl mb-2'>Name</label>
+            <input
+              placeholder="Name"
+              value={ roomName }
+              maxLength={ Constants.ROOM_NAME_MAX }
+              onChange={ handleRoomNameChange }
+              className='w-full border rounded-md p-2 text-[#70422F] mb-2 placeholder:text-amber-700 placeholder:text-opacity-30'
+            />
+          </div>
+          <div className='flex justify-between items-center gap-3'>
+            <label className='min-w-28 text-right text-3xl mb-1'>Max players</label>
+            <select
+              value={ roomMaxPlayers }
+              onChange={ (event) => setRoomMaxPlayers(Number(event.target.value)) }
+              className='w-full border bg-white rounded-md p-2 text-[#70422F] mb-4 placeholder:text-amber-700 placeholder:text-opacity-30'
+            >
+              { PlayersCountList.map(({ value, title }) => (
+                <option key={ value } value={ value }>{ title }</option>
+              )) }
+            </select>
+          </div>
+          <div className='flex justify-between mt-2'>
+            <button className='bg-[#D1714A] text-[#FFEDD7] py-1 px-4 rounded' onClick={ handleCreateRoomClick }>Create room</button>
+            <button className='bg-[#FFEDD7] text-[#70422F] py-1 px-4 rounded' onClick={ () => setIsNewRoom(false) }>Cancel</button>
+          </div>
+        </div>
       ) }
-    </View>
+    </div>
   );
 
   const renderRooms = () => {
     if (!rooms || !rooms.length) {
       return (
-        <View
-          flex
-          center
-          style={ {
-            borderRadius: 8,
-            backgroundColor: '#efefef',
-            color: 'darkgrey',
-            height: 128,
-          } }
-        >
+        <div className='flex items-center justify-center rounded-md h-24 text-2xl bg-[#FFEDD7] bg-opacity-10 font-jmadh'>
           No rooms yet...
-        </View>
+        </div>
       );
     }
 
@@ -190,35 +210,21 @@ const Home: React.FC = () => {
             mode={ metadata.mode }
             onClick={ handleRoomClick }
           />
-          { index !== rooms.length - 1 && <Space size="xxs" /> }
         </React.Fragment>
       );
     });
   };
 
   return (
-    <View flex center style={ { padding: 32, flexDirection: 'column' } }>
-      <View flex center column style={ { width: 700, maxWidth: '100%' } }>
-        <Space size="xs" />
-        <Text style={ { color: 'white', fontSize: 13 } }>
-          An open-source multiplayer game in the browser meant to be hostable, modifiable, and playable by
-          anyone.
-        </Text>
-        <Space size="xxs" />
-      </View>
-
-      <Space size="m" />
-      { renderName() }
-      <Space size="m" />
-      <Box style={ { width: 500, maxWidth: '100%' } }>
-        { renderNewRoom() }
-        <Space size="xxs" />
-        <Separator />
-        <Space size="xxs" />
-        { renderRooms() }
-        <Space size="xxs" />
-      </Box>
-    </View>
+    <div className='bg-bg w-full h-full min-h-screen bg-[#FFE6BD] bg-cover '>
+      <div className='m-auto max-w-md pt-12 gap-4 flex flex-col items-center'>
+        { renderName() }
+        <div className='bg-[#F69C6C] text-[#FFEDD7] shadow-md w-full flex flex-col gap-3 p-6 rounded-md '>
+          { renderNewRoom() }
+          { renderRooms() }
+        </div>
+      </div>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
   content: ["./app/client/index.html", "./app/client/**/*.{css,ts,tsx}"],
   darkMode: ['media', "class"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "bg": "url('./assets/imgs/bg.svg')",
+        "wave": "url('./assets/imgs/wave.svg')",
+        "dice": "url('./assets/imgs/dice.svg')",
+        "face": "url('./assets/imgs/face.svg')",
+        "hand": "url('./assets/imgs/hand.png')",
+        "score": "url('./assets/imgs/score.png')",
+      },
+      fontFamily: {
+        "kreon": ["Kreon", "sans-serif"],
+        "jmadh": ["jmadh", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
